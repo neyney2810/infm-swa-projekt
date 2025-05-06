60e6180df09904186943d71d1b7a7380c70c5e18
Gabriel Jonas Thimm <gthimm47@gmail.com>
2025-05-06 14:00:44 +0200

    Add preprocessing

--- app/components/DataParsing.tsx
+++ app/components/DataParsing.tsx
@@ -2,7 +2,8 @@
 
 import React, { CSSProperties } from "react";
 
-import { useCSVReader } from "react-papaparse";
+import { usePapaParse } from "react-papaparse";
+import { readFileSync } from "fs";
 
 const styles = {
   csvReader: {
@@ -29,10 +30,17 @@ const styles = {
   } as CSSProperties
 };
 
-export default function CSVReader() {
-  const { CSVReader } = useCSVReader();
+const existingHeaders = "Tabelle: 32421-0011\nVerwendung, Einfuhr und Ausfuhr klimawirksamer Stoffe: Bundesl�nder, Jahre, Wirtschaftszweig des Unternehmens, Stoffgruppen;;;;;;;;;;\n"+"Erhebung bestimmter klimawirksamer Stoffe;;;;;;;;;;\n"+ ";;;;;Verwendung klimawirksamer Stoffe;Verwendung klimawirksamer Stoffe (CO2-�quivalente);Einfuhr klimawirksamer Stoffe;Einfuhr klimawirksamer Stoffe (CO2-�quivalente);Ausfuhr klimawirksamer Stoffe;Ausfuhr klimawirksamer Stoffe (CO2-�quivalente)\n;;;;;t;1000 t;t;1000 t;t;1000 t" //TODO: Find regex for this
+const targetHeaders = "jahr;bundesland;kennummer_wz;wirtschaftszweig;stoffgruppe;verwendung;verwendung_co2aequi;einfuhr;einfuhr_co2aequi;ausfuhr;ausfuhr_co2aequi"
+const existingFooters= "__________\n� Statistisches Bundesamt (Destatis), 2025\nStand: 09.01.2025 / 19:19:53"
+const targetFooters = ""
+const zeroValue = ";-"
+const zeroString = ";0"
 
-  return (
+export function CSVReader(data: string)  {
+  const { readString } = usePapaParse();
+  
+  /*return (
     <CSVReader
       onUploadAccepted={(results: any) => {
         alert(JSON.stringify(results));
@@ -52,5 +60,26 @@ export default function CSVReader() {
         </>
       )}
     </CSVReader>
-  );
+  );*/
+  return new Promise (function (complete, error) {
+     readString(data ,{complete, error, delimiter:";", dynamicTyping: true, header: true});
+    }) 
+}
+
+export function prepareHeader(file: string) {
+  var rawData = readFileSync(file , "utf8");
+  var usableData = rawData.replace(existingHeaders, targetHeaders).replace(existingFooters, targetFooters).replace(zeroValue, zeroString);
+  return usableData;
+}
+
+export function ReadCSVFromServer() 
+{
+  var data = prepareHeader("E:\\Github-Projekte\\infm-swa-projekt\\public\\utils\\32421-0011_00 (2).csv")
+  return CSVReader(data).then(function(results) {
+    alert(JSON.stringify(results)); /*replace with display function*/})
+}
+
+export default function DisplayButton()
+{
+  return <button type="button" {...ReadCSVFromServer()}>Read and alert</button>
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import ReadCSVFromServer from "./components/DataParsing";
+import DisplayButton from "./components/DataParsing";
 import CSVReader from "./components/DataParsing";
 import Map from "./components/Map";
 
@@ -5,8 +7,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <CSVReader />
-        <Map />
+        <DisplayButton />
       </main>
     </div>
   );
